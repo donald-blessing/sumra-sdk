20c3c025a9d83cb25e14bdf4e14dfbc808a6106e
Donald Blessing <donaldblessing9@gmail.com>
2022-06-04 08:56:55 +0100

    Added new middleware

---
+++ src/Middleware/CheckMSMiddleware.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Sumra\SDK\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class CheckMSMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure $next
+     *
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $microservices = explode(',', env('SUMRA_MS', ''));
+//        dd($request->header('app-id'));
+        if (empty($microservices) || !in_array($request->header('app-id', null), $microservices)) {
+            return response()->jsonApi([
+                'type' => 'danger',
+                'title' => 'Access error',
+                'message' => "You have not permissions to access this service",
+            ], 403);
+        }
+
+        return $next($request);
+    }
+}
