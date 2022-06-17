a938869a463d6905fa894a852c3f7f074de3d81c
Donald Blessing <donaldblessing9@gmail.com>
2022-06-17 17:10:33 +0100

    Added AdminManagerService

---
+++ database/migrations/2022_04_22_202932_create_admins_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdminsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('phone')->unique();
+            $table->enum('role', ['admin', 'super admin'])->default('admin');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->index(['email', 'phone']);
+            $table->unique(['email', 'phone']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+}
